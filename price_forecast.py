7667dc723b651d465520f9d05fd156e393b7c587
g47193 <johan.lundquist@nordea.com>
2018-03-22 08:35:19 -0100

    dim 4 issue in RandomForrestRegressor

--- price_forecast.py
+++ price_forecast.py
@@ -1,138 +1,41 @@
-# encoding: utf-8
 # module price_forecast.py
-# https://machinelearningmastery.com/handle-missing-data-python/
 
 # imports
 from __future__ import print_function
 from __future__ import division
 
+from model_helper import load_test_case, divide_data
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import export_graphviz
-#import pydot
 import pandas as pd
 import random
-import time
 import datetime
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-# path fix
-#os.environ["PATH"] += os.pathsep + "C:\\Program Files (x86)\\Graphviz2.38\\bin\\"
-
 # functions
 
-def plot_var_importance():
-    # Set the style
-    plt.style.use('fivethirtyeight')
-    # list of x locations for plotting
-    x_values = list(range(len(importances)))
-    # Make a bar chart
-    plt.bar(x_values, importances, orientation='vertical')
-    # Tick labels for x axis
-    plt.xticks(x_values, feature_list, rotation='vertical')
-    # Axis labels and title
-    plt.xlabel('Variable')
-    plt.title('Variable Importances')
-    plt.show()
-
-def plot_predicted_val():
-    # Dates of training values
-    months = features[:, feature_list.index('month')]
-    days = features[:, feature_list.index('day')]
-    years = features[:, feature_list.index('year')]
-    # List and then convert to datetime object
-    dates = [str(int(year)) + '-' + str(int(month)) + '-' + str(int(day)) for year, month, day in
-             zip(years, months, days)]
-    dates = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in dates]
-    # Dataframe with true values and dates
-    true_data = pd.DataFrame(data={'date': dates, 'actual': labels})
-    # Dates of predictions
-    months = test_features[:, feature_list.index('month')]
-    days = test_features[:, feature_list.index('day')]
-    years = test_features[:, feature_list.index('year')]
-    # Column of dates
-    test_dates = [str(int(year)) + '-' + str(int(month)) + '-' + str(int(day)) for year, month, day in
-                  zip(years, months, days)]
-    # Convert to datetime objects
-    test_dates = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in test_dates]
-    # Dataframe with predictions and dates
-    predictions_data = pd.DataFrame(data={'date': test_dates, 'prediction': predictions})
-    # Plot the actual values
-    plt.plot(true_data['date'], true_data['actual'], 'b-', label='actual')
-    # Plot the predicted values
-    plt.plot(predictions_data['date'], predictions_data['prediction'], 'ro', label='prediction')
-    plt.xticks(rotation='60')
-    plt.legend()
-    # Graph labels
-    plt.xlabel('Date')
-    plt.ylabel('Maximum Temperature (F)')
-    plt.title('Actual and Predicted Values')
-    plt.show()
+def print_shapes():
+    print("Training Features Shape:", train_features.shape)
+    print("Training Labels Shape:", train_labels.shape)
+    print("Test Features Shape:", test_features.shape)
+    print("Test Labels Shape:", test_labels.shape)
 
 if __name__ == "__main__":
-    #start_time = time.time()
     pd.set_option('expand_frame_repr', False)
 
-    features = pd.read_csv("C:\\Users\\g47193\\PycharmProjects\\MachineLearning\\data\\aapl_1.csv")
+    # Load from common model_helper
+    out = load_test_case()
+    X = out['X']
+    Y = out['Y']
+    pos = out['Pos']
 
-    #print(features[:5])
-
-
-    print("The shape of our features is:", features.shape)
-
-    # Descriptive statistics for each column
-    #print(features.describe())
-    #plt.plot(features['temp_2'])
-    #plt.show()
-
-    # One-hot encode the data using pandas get_dummies
-    #features = pd.get_dummies(features)
-
-    # Display the first rows of the last 12 columns
-    #print(features.iloc[:,5:][:5])
-
-    # Use numpy to convert to arrays
-    # Labels are the values we want to predict
-    labels = np.array(features['Close'])
-    #labels = np.array(features['actual'])
-    #print(labels)
-
-    # Remove the labels from the features
-    # axis 1 refers to the columns
-    #features = features.drop(['Close'], axis=1)
-    features = features.drop(['Close'], axis=1)
-    features = features.drop(['Open'], axis=1)
-    features = features.drop(['Return'], axis=1)
-    features = features.drop(['Year'], axis=1)
-    features = features.drop(['Month'], axis=1)
-    features = features.drop(['Day'], axis=1)
-
-    # Saving feature names for later use
-    feature_list = list(features.columns)
-
-    # Convert to numpy array
-    features = np.array(features)
-
-    # Split the data into training and testing sets Using Skicit-learn
-    train_features, test_features, train_labels, test_labels = \
-        train_test_split(features, labels, test_size=0.25, random_state = 42)
-
-    #print("Training Features Shape:", train_features.shape)
-    #print("Training Labels Shape:", train_labels.shape)
-    #print("Test Features Shape:", test_features.shape)
-    #print("Test Labels Shape:", test_labels.shape)
-
-    # https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
-    # The baseline predictions are the historical averages
-    # baseline_preds = test_features[:,feature_list.index('average')]
-
-    # Baseline errors, and display average baseline error
-    # baseline_errors = abs(baseline_preds - test_labels)
-
-    # print("Average baseline error: ", round(np.mean(baseline_errors), 2), "degrees.")
+    # Split the data into training and testing sets common model_helper
+    train_features, test_features, train_labels, test_labels = divide_data(X, Y)
+    print_shapes()
 
     # Instantiate model with 1000 decision trees, set n_jobs = -1 = number of cores
     rf = RandomForestRegressor(n_estimators=1000, n_jobs=-1, random_state=42)
@@ -191,5 +94,3 @@ if __name__ == "__main__":
 
     #plot_var_importance()
     #plot_predicted_val()
-
-    #print("Duration in Seconds:", (time.time() - start_time))
