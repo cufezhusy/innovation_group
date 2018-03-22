9087d1fe6e692be092db320b8e04a394a1acc830
Shengyao Zhu <shengyao.zhu@nordea.com>
2018-03-22 12:04:12 +0100

    add some plot functions

--- model_helper.py
+++ model_helper.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 import pickle
 import random
+import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 def load_test_case():
     with open('test_case.pickle', 'rb') as fp:
@@ -12,6 +14,11 @@ def divide_data(X,Y):
     return train_test_split(X, Y,test_size = 0.05, random_state = 42)
 
 
+def model_diff(predict,test_y):
+    temp = (predict - test_y)
+    return np.nan_to_num(temp)
+
+
 if __name__ == '__main__':
     out = load_test_case()
     X = out['X']
@@ -19,4 +26,15 @@ if __name__ == '__main__':
     pos = out['Pos']
     X_train, X_test, y_train, y_test= divide_data(X,Y)
 
-    print(sum(y_test)/len(y_test))
+    y_predict = y_test*1.02
+    plt.hist(model_diff(y_predict,y_test))
+    plt.title("Gaussian Histogram")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+
+    fig = plt.gcf()
+    fig.show()
+
+
+    plt.plot(y_predict,y_test)
+    fig.show()
